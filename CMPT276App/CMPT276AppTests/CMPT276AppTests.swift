19d12fb56168c274ad27772f38a09fd828ffd0b9
Michael Lee <micrlee@Michael-Lees-iMac.local>
2018-06-20 21:13:51 -0700

    Base application: page template app

---
+++ CMPT276App/CMPT276AppTests/CMPT276AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  CMPT276AppTests.swift
+//  CMPT276AppTests
+//
+//  Created by Michael Lee on 2018-06-20.
+//  Copyright © 2018 Nurture. All rights reserved.
+//
+
+import XCTest
+@testable import CMPT276App
+
+class CMPT276AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

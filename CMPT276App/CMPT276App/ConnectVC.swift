0641709e30140c0570fce9b5630368e7786fb13c
Michael Lee <micrlee@MichaelLeesiMac.hitronhub.home>
2018-06-27 21:55:28 -0700

    started making connect forum

---
+++ CMPT276App/CMPT276App/ConnectVC.swift
@@ -0,0 +1,48 @@
+//
+//  ConnectVC.swift
+//  CMPT276App
+//
+//  Created by Michael Lee on 2018-06-27.
+//  Copyright © 2018 Nurture. All rights reserved.
+//
+
+import UIKit
+
+class ConnectVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        }
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       return UITableViewCell()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
